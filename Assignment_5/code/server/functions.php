a3930bd1cf182df94b58955dc345f55054fcf249
vitid <view.cp32@gmail.com>
2017-04-20 23:44:05 -0700

    Add report.pdf and related functions

--- Assignment_5/code/server/functions.php
+++ Assignment_5/code/server/functions.php
@@ -1,16 +1,27 @@
 <?php  
-function getSnippet($search_terms,$doc_id){
-	$myfile = fopen("files/".$doc_id, "r");
-	while(!feof($myfile)) {
-	  $line = fgets($myfile);
-	  $terms = explode(" ",$line);
-	  if(sizeof(array_intersect($search_terms,$terms)) > 0){
-	  	echo $line;
-	  	break;
-	  }
+function getSnippet($search_terms,$doc_id,$expand=100){
+	$content = file_get_contents("files/".$doc_id,true);
+	$content = strip_tags($content);
+	$content = preg_replace("!\s+!"," ",$content);
+
+	$snippet = "";
+	$pattern = "/[^a-zA-Z]" . implode("[^a-zA-Z]|[^a-zA-Z]",$search_terms) . "[^a-zA-Z]/i";
+	$index = -1;
+	if(preg_match($pattern, $content, $matches, PREG_OFFSET_CAPTURE)) {
+	    $index = $matches[0][1];
+	    $index += 1;//adjust for one whitespace
+	    $start_index = max(0,$index - $expand);
+	    $snippet = substr($content,$start_index,2*$expand);
 	}
-	fclose($myfile);
+	return $snippet;
 }
 
-getSnippet(["2018"],"32caf0f7-c248-4d05-8bdd-bab791d267a8.html");
+function decorateSnippet($search_terms,$content){
+	foreach($search_terms as $term){
+		$content = preg_replace('/'.$term.'/i',"<span class='snippet'>\$0</span>",$content);
+	}
+	return $content;
+}
+#echo getSnippet(["visualizing"],"32caf0f7-c248-4d05-8bdd-bab791d267a8.html");
+#echo decorateSnippet(["hello"]," hello world hellox Hello ");
 ?>
--- Assignment_5/code/server/query.php
+++ Assignment_5/code/server/query.php
@@ -1,10 +1,71 @@
 <?php
 ini_set('memory_limit', '-1');
 include 'spellcorrector/SpellCorrector.php';
+include 'functions.php';
 
 // make sure browsers see this page as utf-8 encoded HTML
 header('Content-Type: text/html; charset=utf-8');
+?>
+<head>
+  <link rel="stylesheet"
+  href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
+  <script
+  src="https://code.jquery.com/jquery-1.12.4.js"
+  integrity="sha256-Qw82+bXyGq6MydymqBxNPYTaUXXq7c8v3CwiYwLLNXU="
+  crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" 
+  integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" 
+  crossorigin="anonymous"></script>
+  <style>
+  .snippet{
+    color: red;
+  }
+  </style>
+  <script type="text/javascript">
+  function getSuggestion(term,e){
+    //detect KeyUp and KeyDown
+    if(e.keyCode == 38 || e.keyCode == 40){
+      return;
+    }
 
+    if(term.length == 0 || term[term.length-1] == " "){
+      return;
+    }
+
+    terms = term.split(" ");
+    search_term = terms[terms.length-1].toLowerCase();
+    var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            //query is changed
+            if(document.getElementById("q").value != term){
+              return;
+            }
+            var response = JSON.parse(this.responseText);
+            var suggestions = response["suggest"]["suggest"][search_term]["suggestions"];
+            var limit = Math.min(10,suggestions.length);
+            if(search_term.length >= 2){
+              limit = Math.min(7,limit)
+            }
+            var suggestion_list = [];
+            for(var i = 0;i<limit;++i){
+              suggestion_list.push(term.substr(0,term.lastIndexOf(" ")+1) + suggestions[i]["term"]);
+            }
+
+            $("#q").autocomplete({
+              source: function( request, response ) {
+                      response(suggestion_list);
+                  }
+            });
+          }
+      };
+      xmlhttp.open("GET", "suggest_service.php?q=" + search_term, true);
+      xmlhttp.send();
+
+  }
+  </script>
+</head>
+<?php
 $limit = 10;
 $query = isset($_REQUEST['q']) ? $_REQUEST['q'] : false;
 $results = false;
@@ -45,72 +106,23 @@ if ($query)
 }
 
 ?>
-<html>
-  <head>
-    <title>PHP Solr Client Example</title>
-    <script type="text/javascript">
-    	function getSuggestion(term,e){
-    		//detect KeyUp and KeyDown
-    		if(e.keyCode == 38 || e.keyCode == 40){
-    			return;
-    		}
-
-    		var candidate_obj = document.getElementById("candidate_words");
-    		candidate_obj.innerHTML = "";
-    		term = term.trim();
-
-    		if(term.length > 0){
-    			terms = term.split(" ").filter(function(x){return x.length > 0;});
-    			search_term = terms[terms.length-1];
-    			var xmlhttp = new XMLHttpRequest();
-        		xmlhttp.onreadystatechange = function() {
-	            if (this.readyState == 4 && this.status == 200) {
-	            		//query is changed
-	            		if(document.getElementById("q").value.trim() != term){
-	            			return;
-	            		}
-	            		var response = JSON.parse(this.responseText);
-	            		var suggestions = response["suggest"]["suggest"][search_term]["suggestions"];
-	            		var limit = Math.min(10,suggestions.length);
-	            		if(search_term.length >= 2){
-	            			limit = Math.min(7,limit)
-	            		}
-	            		for(var i = 0;i<limit;++i){
-
-	            			terms[terms.length-1] = suggestions[i]["term"];
-	            			var option_element = document.createElement("option");
-    						option_element.value = terms.join(" ");
-    						candidate_obj.appendChild(option_element);
-	            		}
-		            }
-		        };
-		        xmlhttp.open("GET", "suggest_service.php?q=" + search_term, true);
-		        xmlhttp.send();
-    		}
-
-    	}
-    </script>
-  </head>
-  <body>
+  <body class="ui-widget">
     <form  accept-charset="utf-8" method="get">
       <label for="q">Search:</label>
-      <input id="q" name="q" type="text" value="<?php echo htmlspecialchars($query, ENT_QUOTES, 'utf-8'); ?>" list="candidate_words" onkeyup="getSuggestion(this.value,event)"/>
+      <input id="q" name="q" type="text" value="<?php echo htmlspecialchars($query, ENT_QUOTES, 'utf-8'); ?>" onkeyup="getSuggestion(this.value,event)" autocomplete="off"/>
       <input type="submit"/>
     </form>
-    <datalist id="candidate_words">
-      <option value="airplane"/>
-      <option value="airplne"/>
-      <option value="hello"/>
-    </datalist>
     <?php 
       if($query){
         
+        $query = strtolower($query);
+
         $terms = explode(" ",$query);
         $correct_terms = array();
         $is_spell_error = false;
         for($i=0;$i<sizeof($terms);++$i){
         	$term = $terms[$i];
-        	$correct_term = SpellCorrector::correct($term);
+        	$correct_term = strtolower(SpellCorrector::correct($term));
         	if($term != $correct_term){
         		$is_spell_error = true;
         	}
@@ -119,7 +131,7 @@ if ($query)
         if($is_spell_error){
         	$correct_terms = implode(" ",$correct_terms);
         	?>
-        	You may refer to:<a href="query.php?q=<?=$correct_terms?>"><?= $correct_terms; ?></a>
+        	Did you mean: <a href="query.php?q=<?=$correct_terms?>"><?= $correct_terms; ?></a>
         	<?php
         }
       }
@@ -139,7 +151,7 @@ if ($results)
   {
 ?>
       <li>
-        <table width="100%">
+        <table width="70%">
         <tr>
         <td width="50%" valign="top">
           <table width="100%" style="border: 1px solid black; text-align: left">
@@ -151,25 +163,13 @@ if ($results)
             $url = $dict[$real_id];
             ?>
             <tr>
-                <th>ID</th>
-                <td>
-                <?php echo substr($doc->id,0,strripos($doc->id,"/")+1) . "<b>" . $real_id . "</b>" ?>
-                </td>
-            </tr>
-            <tr>
-                <th>Title</th>
                 <td><a href='<?php echo $url ?>'><?php echo htmlspecialchars($doc->title, ENT_NOQUOTES, 'utf-8'); ?></a></td>
             </tr>
             <tr>
-                <th>Description</th>
                 <td><?php 
-                  $description = "";
-                  if(is_array($doc->description)){
-                    $description = $doc->description[0];
-                  }else{
-                    $description = $doc->description;
-                  }
-                  echo htmlspecialchars($description, ENT_NOQUOTES, 'utf-8'); 
+                  $content = getSnippet($terms,$real_id);
+                  $content = decorateSnippet($terms,$content);
+                  echo $content;
                 ?></td>
             </tr>
             <?php
